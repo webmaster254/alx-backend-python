410e1aa339b85fd622afac80ad0d098820b00bcc
webmaster254 <josephm2800@gmail.com>
2025-05-31 21:05:24 +0300

    refactor: split user creation logic into private _create_user method

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,14 +7,22 @@ from django.core.validators import MinValueValidator, MaxValueValidator, RegexVa
 
 class UserManager(BaseUserManager):
     """Custom user model manager where email is the unique identifier"""
-    def create_user(self, email, password=None, **extra_fields):
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a user with the given email and password."""
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError('The Email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular user with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
         """Create and save a SuperUser with the given email and password."""
